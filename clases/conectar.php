cf50bac13d8e86d1177e42e3182cd441d5283bcf
klever <jnarklever123@hotmail.com>
2017-12-11 11:11:20 -0500

    inicio

--- clases/conectar.php
+++ clases/conectar.php
@@ -6,7 +6,7 @@
 		//funcion publica conectar
 		public function conectar(){
 			//utilizar la clase msqli p' conectarnos
-			$this->cn=new mysqli('localhost','root','','amazon');
+			$this->cn=new mysqli('us-cdbr-iron-east-05.cleardb.net','ba78fed948b3f9','4bf7c3d3','heroku_bed8d7e563b7b30');
 			//retornamos (devolver) la conexion
         	return $this->cn;
 		}
